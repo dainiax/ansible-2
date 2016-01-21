6961f8015841cd3d19e60e1d27a8ce26a3ce6699
Dainius <dainiax@gmail.com>
2016-01-21 12:01:59 +0000

    Create runtime.py

---
+++ runtime.py
@@ -0,0 +1,29 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from datetime import datetime
+from ansible.plugins.callback import CallbackBase
+
+class CallbackModule(CallbackBase):
+    """
+    Shows current time and how long your each play ran for.
+    """
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'aggregate'
+    CALLBACK_NAME = 'runtime'
+    CALLBACK_NEEDS_WHITELIST = False
+
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+        self.start_time = datetime.now()
+
+    def time_hours_minutes_seconds(self, runtime):
+        minutes = (runtime.seconds // 60) % 60
+        r_seconds = runtime.seconds - (minutes * 60)
+        current_time = datetime.now()
+        return current_time, runtime.seconds // 3600, minutes, r_seconds
+
+    def v2_on_any(self, *args, **kwargs):
+        end_time = datetime.now()
+        runtime = end_time - self.start_time
+        self._display.display("[%s] - %sh %sm %ss" % (self.time_hours_minutes_seconds(runtime)))
