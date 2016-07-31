8de2beb54ecb2512ea2b328097c7a581e50e6c5a
dainiax-ultra <dainiax@gmail.com>
2016-07-31 23:23:03 +0300

    .gitignore updated

--- plugins/callback_plugins/runtime.py
+++ plugins/callback_plugins/runtime.py
@@ -1,8 +1,8 @@
 from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
 from datetime import datetime
 from ansible.plugins.callback import CallbackBase
+__metaclass__ = type
+
 
 class CallbackModule(CallbackBase):
     """
